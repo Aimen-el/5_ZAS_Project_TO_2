e787f14b10575d60b08dca536f37682802501ce8
zakafir <zak.afir@gmail.com>
2017-04-26 12:00:09 +0200

    Send notification

--- app/src/main/java/com/semantic/ecare_android_v2/ui/Proximity.java
+++ app/src/main/java/com/semantic/ecare_android_v2/ui/Proximity.java
@@ -1,11 +1,17 @@
 package com.semantic.ecare_android_v2.ui;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
 import android.util.Log;
 import android.widget.Toast;
+import android.support.v4.app.NotificationCompat;
+
+
+import com.semantic.ecare_android_v2.R;
 
 import static com.semantic.ecare_android_v2.ui.NoteDialogActivity.ARRAY_LAT_LNG_PATIENT;
 import static com.semantic.ecare_android_v2.ui.NoteDialogActivity.note;
@@ -24,14 +30,35 @@ public class Proximity extends BroadcastReceiver {
 
             for(int i = 0; i<=mBoundService.getPatientList().size();i++){
                 if(note.getAddress().equals(mBoundService.getPatientList().get(i).getAddress())){
+                    Log.d("you're in the zone ",ARRAY_LAT_LNG_PATIENT.toString());
                     Log.d("Vous êtes arrivé chez le "+mBoundService.getPatientList().get(i).getName(),ARRAY_LAT_LNG_PATIENT.toString());
-                    Toast.makeText(context,mBoundService.getPatientList().get(i).getName(),Toast.LENGTH_LONG).show();
+                    Toast.makeText(context,"Vous êtes arrivé chez le "+mBoundService.getPatientList().get(i).getName(),Toast.LENGTH_LONG).show();
                     break;
                 }
             }
+            addNotification(context);
         } else {
             Log.d("you're out of the zone ",ARRAY_LAT_LNG_PATIENT.toString());
-            //Toast.makeText(context,"you're out of the zone",Toast.LENGTH_SHORT).show();
+            Toast.makeText(context,"you're out of the zone",Toast.LENGTH_SHORT).show();
         }
     }
+
+    private void addNotification(Context context)
+    {
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.icon_error)
+                        .setContentTitle("C'est votre arrivée")
+                        .setContentText("Vous êtes arrivés chez le patient ");
+
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+
+        // Add as notification
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(0, builder.build());
+    }
+
 }
